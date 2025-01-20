e7f0a2eafa6fed6791cf7180e268ef3419a384c1
Marius Andra <marius.andra@gmail.com>
2025-01-20 23:23:40 +0100

    other sources

--- src/state/initial-state.ts
+++ src/state/initial-state.ts
@@ -1,47 +1,64 @@
 // Portions of this file are Copyright 2021 Google LLC, and licensed under GPL2+. See COPYING.
 
-import defaultScad from './default-scad.ts';
-import { State } from './app-state.ts';
+// import defaultScad from "./default-scad.ts";
+import { Source, State } from "./app-state.ts";
 
-export const defaultSourcePath = '/playground.scad';
-export const defaultModelColor = '#f9d72c';
-const defaultBlurhash = "|KSPX^%3~qtjMx$lR*x]t7n,R%xuxbM{WBt7ayfk_3bY9FnAt8XOxanjNF%fxbMyIn%3t7NFoLaeoeV[WBo{xar^IoS1xbxcR*S0xbofRjV[j[kCNGofxaWBNHW-xasDR*WTkBxuWBM{s:t7bYahRjfkozWUadofbIW:jZ";
-  
-export function createInitialState(state: State | null, source?: {content?: string, path?: string, url?: string, blurhash?: string}): State {
+import models from "../models.json";
 
-  type Mode = State['view']['layout']['mode'];
-  
-  const mode: Mode = window.matchMedia("(min-width: 768px)").matches 
-    ? 'multi' : 'single';
+const defaultScad = models["mark1.scad"];
+export const defaultSourcePath = "/mark1.scad";
+export const defaultModelColor = "#f9d72c";
+// const defaultBlurhash =
+//   "|KSPX^%3~qtjMx$lR*x]t7n,R%xuxbM{WBt7ayfk_3bY9FnAt8XOxanjNF%fxbMyIn%3t7NFoLaeoeV[WBo{xar^IoS1xbxcR*S0xbofRjV[j[kCNGofxaWBNHW-xasDR*WTkBxuWBM{s:t7bYahRjfkozWUadofbIW:jZ";
+
+export function createInitialState(
+  state: State | null,
+  source?: { content?: string; path?: string; url?: string; blurhash?: string }
+): State {
+  type Mode = State["view"]["layout"]["mode"];
+
+  const mode: Mode = window.matchMedia("(min-width: 768px)").matches
+    ? "multi"
+    : "single";
 
   let initialState: State;
   if (state) {
-    if (source) throw new Error('Cannot provide source when state is provided');
+    if (source) throw new Error("Cannot provide source when state is provided");
     initialState = state;
   } else {
     let content, path, url, blurhash;
-    if (source) {
-      content = source.content;
-      path = source.path;
-      url = source.url;
-      blurhash = source.blurhash;
-    } else {
-      content = defaultScad;
-      path = defaultSourcePath;
-      blurhash = defaultBlurhash;
-    }
-    let activePath = path ?? (url && new URL(url).pathname.split('/').pop()) ?? defaultSourcePath;
+    // if (source) {
+    //   content = source.content;
+    //   path = source.path;
+    //   url = source.url;
+    //   blurhash = source.blurhash;
+    // } else {
+    //   content = models["mark1.scad"];
+    //   path = defaultSourcePath;
+    //   blurhash = defaultBlurhash;
+    // }
+
+    const sources: Source[] = Object.entries(models).map(([path, content]) => ({
+      path: `/${path}`,
+      content,
+    }));
+
+    let activePath =
+      path ??
+      (url && new URL(url).pathname.split("/").pop()) ??
+      defaultSourcePath;
+
     initialState = {
       params: {
         activePath,
-        sources: [{path: activePath, content, url}],
+        sources,
         features: [],
-        exportFormat2D: 'svg',
-        exportFormat3D: 'glb',
+        exportFormat2D: "svg",
+        exportFormat3D: "glb",
       },
       view: {
         layout: {
-          mode: 'multi',
+          mode: "multi",
           editor: true,
           viewer: true,
           customizer: false,
@@ -49,25 +66,27 @@ export function createInitialState(state: State | null, source?: {content?: stri
 
         color: defaultModelColor,
       },
-      preview: blurhash ? {blurhash} : undefined,
+      preview: blurhash ? { blurhash } : undefined,
     };
   }
 
   if (initialState.view.layout.mode != mode) {
-    if (mode === 'multi' && initialState.view.layout.mode === 'single') {
+    if (mode === "multi" && initialState.view.layout.mode === "single") {
       initialState.view.layout = {
         mode,
         editor: true,
         viewer: true,
-        customizer: initialState.view.layout.focus == 'customizer'
-      }
-    } else if (mode === 'single' && initialState.view.layout.mode === 'multi') {
+        customizer: initialState.view.layout.focus == "customizer",
+      };
+    } else if (mode === "single" && initialState.view.layout.mode === "multi") {
       initialState.view.layout = {
         mode,
-        focus: initialState.view.layout.viewer ? 'viewer'
-          : initialState.view.layout.customizer ? 'customizer'
-          : 'editor'
-      }
+        focus: initialState.view.layout.viewer
+          ? "viewer"
+          : initialState.view.layout.customizer
+          ? "customizer"
+          : "editor",
+      };
     }
   }
 
@@ -77,11 +96,11 @@ export function createInitialState(state: State | null, source?: {content?: stri
   // if (initialState.params.sourcePath !== defaultSourcePath) {
   //   fs.writeFile(defaultSourcePath, defaultScad);
   // }
-  
-  const defaultFeatures = ['lazy-union'];
-  defaultFeatures.forEach(f => {
+
+  const defaultFeatures = ["lazy-union"];
+  defaultFeatures.forEach((f) => {
     if (initialState.params.features.indexOf(f) < 0)
-    initialState.params.features.push(f);
+      initialState.params.features.push(f);
   });
 
   return initialState;
@@ -90,6 +109,6 @@ export function createInitialState(state: State | null, source?: {content?: stri
 export function getBlankProjectState() {
   return createInitialState(null, {
     path: defaultSourcePath,
-    content: defaultScad, 
+    content: defaultScad,
   });
 }
